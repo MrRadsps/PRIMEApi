f04b812b2f51e5feed31b12898f5c3bfecd74b78
Mr  Rad <36004865+MrRadsps@users.noreply.github.com>
2018-08-07 19:26:51 +0430

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+token = "---" -- prime bot token
+sudo_add = 495059735 -- prime full sudo
+sudo_users = {
+  495059735, -- prime sudo's
+}
+botnamefa = 'راد پرایم'
+sudoid = '@justice_boy'
+supportgap = 'ثبت نشده'
+channel = '@RadBotNews' 
+channel1 = 'RadBotNews' -- without @
+pvresan = '@Mrunknown_bot'
+hypername = 'RAD NEWS'
+botnameen = 'RAD PRIME'
+
+
